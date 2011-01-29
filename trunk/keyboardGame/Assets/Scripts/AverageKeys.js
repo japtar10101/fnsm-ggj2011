3789990dc3ea36e66df035728c1b46237843115a
Japtar10101@gmail.com <Japtar10101@gmail.com@2525e648-a24f-6fb8-186d-bc4e16fe77ae>
2011-01-29 20:42:04 +0000

    Adding an island scene.

---
+++ trunk/keyboardGame/Assets/Scripts/AverageKeys.js
@@ -0,0 +1,16 @@
+public var row1Keys : KeyCode[];
+public var row2Keys : KeyCode[];
+public var row3Keys : KeyCode[];
+public var row4Keys : KeyCode[];
+
+private var keyHit : boolean = false;
+private var hitTime : float = 0;
+private var xIncrement : float;
+private var zIncrement : float;
+
+function Start() {
+	
+}
+
+function Update () {
+}
