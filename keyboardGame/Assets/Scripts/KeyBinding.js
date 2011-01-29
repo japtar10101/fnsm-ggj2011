2c258284f04df053404b70299b45514a3c307ba3
Japtar10101@gmail.com <Japtar10101@gmail.com@2525e648-a24f-6fb8-186d-bc4e16fe77ae>
2011-01-29 20:42:04 +0000

    Adding an island scene.

---
+++ keyboardGame/Assets/Scripts/KeyBinding.js
@@ -0,0 +1,10 @@
+@script RequireComponent(SphereCollider)
+
+public var key : KeyCode;
+public var average : AverageKeys;
+
+function Update () {
+	var e : Event = Event.current;
+    if ((e.keyCode == key)) {
+    }
+}
