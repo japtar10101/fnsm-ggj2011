26ea3c6a8a9e3c8d2e3d1c45f65838a1b5824bcc
Japtar10101@gmail.com <Japtar10101@gmail.com@2525e648-a24f-6fb8-186d-bc4e16fe77ae>
2011-01-29 16:20:27 +0000

    Hm, I forgot to save the scene again.

--- trunk/keyboardGame/Assets/Scripts/HitEnemy.js
+++ trunk/keyboardGame/Assets/Scripts/HitEnemy.js
@@ -1,6 +1,6 @@
 public var detectDuration : float = 1;
+public var magnifier : float = 2000;
 private var theCollider : SphereCollider;
-private var magnifier : float = 3000;
 private var startTime : float;
 
 function Start() {
@@ -11,20 +11,6 @@ function Start() {
 	}
 	startTime = Time.time;
 }
-
-/*
-function OnCollisionEnter(collision : Collision) {
-	// Go through everything colliding with this explosion
-    for (var contact : ContactPoint in collision.contacts) {
-		// Check if this collider is tagged as an enemy
-		var enemy : Collider = contact.otherCollider;
-		if(enemy.CompareTag("Enemy")) {
-			// TODO: decrease its health
-			enemy.attachedRigidbody.AddForce((transform.position - theCollider.transform.position) * magnifier);
-		}
-    }
-}
-*/
 
 function OnTriggerEnter (enemy : Collider) {
     if(((Time.time - startTime) < detectDuration) && enemy.CompareTag("Enemy")) {
