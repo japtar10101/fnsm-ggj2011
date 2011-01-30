50eb951fda5c6f00dac3b741e41ee53a7321a0ff
ruben.brown@gmail.com <ruben.brown@gmail.com@2525e648-a24f-6fb8-186d-bc4e16fe77ae>
2011-01-30 03:12:36 +0000

    added new ball to ballDestruction.unity

--- trunk/keyboardGame/Assets/testGUIruben.js
+++ trunk/keyboardGame/Assets/testGUIruben.js
@@ -1,14 +1,5 @@
+/* GUI.Label example */
 
-
-// Apply the Skin in our OnGUI() function
 function OnGUI () {
-
-	// Now create any Controls you like, and they will be displayed with the custom Skin
-	GUILayout.Button ("I am a re-Skinned Button");
-
-	// You can change or remove the skin for some Controls but not others
-	GUI.skin = null;
-
-	// Any Controls created here will use the default Skin and not the custom Skin
-	GUILayout.Button ("This Button uses the default UnityGUI Skin");
+	GUI.Label (Rect (25, 25, 100, 30), "Label");
 }
